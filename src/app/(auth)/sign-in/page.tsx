db3d90bd99f23156ce7df79bef36b13d911216a9
Arpan <rockarpan31@gmail.com>
2024-05-21 15:28:09 +0545

    verify code

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,122 @@
-import React from 'react'
+"use client"
 
-export default function page() {
-  return (
-    <div>page</div>
-  )
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as  z from "zod"
+import Link from "next/link"
+import { useDebounceCallback } from 'usehooks-ts'
+import { useToast } from "@/components/ui/use-toast"
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import { signUpSchema } from "@/schemas/signUpSchema"
+import axios, { AxiosError } from 'axios'
+
+import { ApiResponse } from "@/types/ApiResponse"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react"
+import { signInSchema } from "@/schemas/signInSchema"
+import { signIn } from "next-auth/react"
+const page = () => {
+
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const { toast } = useToast()
+    const router = useRouter();
+
+
+    //zod implementation
+
+    const form = useForm<z.infer<typeof signInSchema>>({ //<<optional>> 
+        resolver: zodResolver(signInSchema),
+        defaultValues: {
+            identifier: '',
+            password: ''
+        }
+    })
+    const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+        const result = await signIn('credentials', {
+            redirect: false,
+            identifier: data.identifier,
+            password: data.password
+        })
+        if (result?.error) {
+
+            toast({
+                title: "Login Failed",
+                description: "Somthing is wrong ",
+                variant: "destructive"
+            })
+        }
+        if (result?.url) {
+            router.replace('/dashboard')
+
+        }
+    }
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+                <div className="text-center">
+                    <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+                        Join Anonymous Message
+                    </h1>
+                    <p className="mb-4"> Sign in to start your anonymous adventure</p>
+
+                </div>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+
+                        <FormField
+                            name="identifier"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email/Username</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="email/username" {...field}
+
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            name="password"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Password" {...field}
+
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit" disabled={isSubmitting}>
+                            {
+                                isSubmitting ? (
+                                    <>
+                                        <Loader2 className="mr-2 h-4 w-4 animate-spin" />Please wait
+                                    </>
+                                ) : ('signin')}
+                        </Button>
+
+                    </form>
+                </Form>
+                <div className="text-center mt-4 ">
+                    <p>
+                        Already a member ?{' '}
+                        <Link href="/sign-in" className="text-blue-600 hover:text-blue-800">Sign in</Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
 }
+
+export default page
